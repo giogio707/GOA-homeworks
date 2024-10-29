b6651aca204605c5abb478c94b9025056e995921
giogio707 <giobandzaladze@gmail.com>
2024-10-29 21:28:21 +0400

    first commit

---
+++ Day2/homework2.py
@@ -0,0 +1,25 @@
+#2
+name="gio"
+lastname="bandzaladze"
+print(100 * (name + " " + lastname + " "))
+
+#3
+#კონკადინაცია ნიაშნავს ორი ცვლადის შეერთება
+
+#4
+city = "London"
+print(city)
+
+#მე5 და მე6 არ მისწავლია ჯერ
+
+#7
+int1 = 12
+int2 = 34
+
+print(int1 + int2)
+print(int1 - int2)
+print(int1 * int2)
+print(int1 / int2)
+
+#8
+# კომენტარი ნიშნავს რაღაცის ჩანიშვნას
---
+++ Dey3/lesson3.py
@@ -0,0 +1,40 @@
+#1
+
+points = 50
+print(points)
+points = 55
+print(points)
+points = 60
+print(points)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

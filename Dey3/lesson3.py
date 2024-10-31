916fd009222554a88969a8f3687dfe3fc9d32b71
giogio707 <giobandzaladze@gmail.com>
2024-10-31 19:10:08 +0400

    day3

--- Dey3/lesson3.py
+++ Dey3/lesson3.py
@@ -7,34 +7,51 @@ print(points)
 points = 60
 print(points)
 
+#2
 
+#snake_case ნიშნავს ორ სიტყვიანი ცვლადის სახელის ქვედა ტირით გამოყოფა
+name_lastname = "gio BANDZALADZE"
 
+#3
 
+#name=gio
+name="gio"
 
+#print=(name)
+print(name)
 
+#print("9" + 5)
+print(9 + 5)
 
+#print(102 * "cxra")
+print(102 * 9)
 
+int1=20
+int2=10
+#print"int1 - int2"
+print(int1 - int2)
 
+lastname="bandzaladze"
+#print(5 * name + " " + lastname)
+print(5 * (name + " " + lastname))
 
+#from "turtle" import *
+from turtle import *
 
+#print((name))
+print(name)
 
+#print("int1" "/" "int2")
+print(int1 / int2)
 
+#ball_color=(red)
+ball_color="red"
 
+#4
 
+favourite_number = 999
+age=9
+multiplie = favourite_number * age
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#5
+print((3 + 17) / 2 * 5)

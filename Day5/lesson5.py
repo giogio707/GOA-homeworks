a0f73a5754886068c3b947ada0ce42d3566adde0
giogio707 <giobandzaladze@gmail.com>
2024-11-15 19:40:07 +0400

    Day5

---
+++ Day5/lesson5.py
@@ -0,0 +1,28 @@
+#1
+
+user_input = input('შეიყვანეთ თქვენი საყვარელი სპორტი: ')
+print(user_input)
+
+
+#2
+
+name = input('რა გქვია? ')
+lastname = input('რა გვარი ხარ? ')
+age = input('რამდენი წლის ხარ? ')
+place = input('რომელ ქალაქში ცხოვრობ? ')
+hobby = input('რა არის შენი ჰობი? ')
+
+print('შენი სახელი და გვარია: ' + name + ' ' + lastname + ' ხარ ' + age + ' წლის, ' + 'ცხოვრობ ქალაქ ' + place + ", " ' შენი ჰობია: ' + hobby)
+
+#3
+
+num1 = int(input('შეიყვანეთ პირველი რიცხვი: '))
+num2 = int(input('შეიყვანეთ მეორე რიცხვი: '))
+sum=num1 + num2
+print('ჯამი = ' + sum)
+
+difference=num1 - num2
+print('სხვაობა = ' + difference)
+
+multiplied = num1 * num2
+print('ნამრავლი = ' + multiplied)

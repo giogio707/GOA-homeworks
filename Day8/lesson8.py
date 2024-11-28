8f2e614000f4437c2f3770e471971629a962b6db
giogio707 <giobandzaladze@gmail.com>
2024-11-28 18:29:17 +0400

    homework1

---
+++ Day8/lesson8.py
@@ -0,0 +1,22 @@
+#1
+number = 7
+number2 = 5.4
+name = "Tom"
+bool(False)
+
+#2
+age=int(input("enter your age"))
+a=type(age)
+print(a)
+
+#3
+age2=int(input("enter your age"))
+print('in 5 years you will be ' + age2 + 5)
+
+#4
+name2=input('enter your name: ')
+lastname=input('enter your lastname:')
+age3=int(input('enter your age: '))
+height = int(input('eneter your height: '))
+color = input('eneter your favourite color: ')
+print(name2 + lastname + age3 + height + color)

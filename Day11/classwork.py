1fb3560213993112a9e6a788fd2e29c8531ddb44
giogio707 <giobandzaladze@gmail.com>
2024-12-13 23:49:50 +0400

    Day01

---
+++ Day11/classwork.py
@@ -0,0 +1,25 @@
+#1
+age = input("enter your age")
+height = input("enter your height")
+age = "5"
+height = "1.54"
+print(1.54 < 5)
+print(5 > 1.54)
+
+
+#2
+#== ნიშნავს რომ რაღა უდრის რაღაც რიცხვს.
+#> ეს ნიშნავს რომ პირველი რიცხვი მეტია მეორეზე.
+#< ეს ნიშნავს რომ პირველი რიცხვი მეორეზე ნაკლებია.
+
+
+#3
+age = input("enter your age")
+height = input("enter your height")
+print(age)
+print(height)
+print(5 + 1.54)
+print(5 - 1.54)
+print(5 + 1.54)
+print(5 // 1.54)
+print(5 * 1.54)

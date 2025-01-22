a2ba8ee1aa7b4ad07faf5ce4e67b4a0cec1256d2
giogio707 <giobandzaladze@gmail.com>
2025-01-22 21:31:47 +0400

    Day16

---
+++ Day16/homework/lesson16.py
@@ -0,0 +1,23 @@
+#2
+for i in range(10): 
+ print(i)
+
+#3
+for i in range(50,150):
+ print(i)
+
+#6
+
+#1
+print(12==12)
+print(56>54)
+print(90>99)
+print(123>222)
+print(435 > 1)
+
+#2
+print(1>3 and 5>2)#False
+print(12<13 or 14>15)#True
+print(999<1000 and 55>54)#True
+print(22>21 or 555>444)#True
+print(78>77 and 0>1)#False

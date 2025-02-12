c5dbe9301921cc3a742789b55db7b8a7c2b667b2
giogio707 <giobandzaladze@gmail.com>
2025-02-12 22:31:14 +0400

    second commit

--- Day18/homework/lesson.py
+++ Day18/homework/lesson.py
@@ -12,14 +12,9 @@ for i in range(20,0,-1):
 
 
 #4
-for i in range(50):
+for i in range(1,51):
   print(i)
 
 #5
-for i in range(2,10,+2):
-  print(i)
-
-#6
-num = int(input("Enter a num:"))
-for i in range(0,num):
+for i in range(0,11,2):
   print(i)

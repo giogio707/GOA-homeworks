f3b673daae545fbb235946874701b7d876b4d0c8
giogio707 <giobandzaladze@gmail.com>
2025-03-03 14:06:26 +0400

    second commit

---
+++ Day22/classwork/lesson.py
@@ -0,0 +1,7 @@
+#elif statement არის if/else-ის დამატებითი პირობა, და აღნიშნავს "სხვა შემთხვევაში თუ ესეა...."
+
+
+
+x=int(input("enter a number"))
+if x==20:
+    print("yes")
---
+++ Day22/homework/lesson.py
@@ -0,0 +1,10 @@
+#2 მოცემული კოდი გადაიტანეთ Vscode-ში და გაასწორეთ შეცდომები, რათა სწორად იმუშაოს:
+
+status = "student"
+if status == "student":
+   print(True)
+else:
+   print(False)
+
+
+   
